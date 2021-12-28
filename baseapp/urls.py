90fc225ae9e1260794f2dc408d7aaea04398c9cb
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2021-12-28 19:51:51 +0600

    General Git Push

--- baseapp/urls.py
+++ baseapp/urls.py
@@ -6,5 +6,11 @@ app_name = "baseapp"
 
 urlpatterns = [
     path('', views.homePage, name="homepage"),
+    path('my-profile-tab', views.myProfileTab, name='my-profile-tab'),
+    path('guilds-tab/', views.guildsTab, name='guilds-tab'),
+    path('challenges-tab/', views.challengesTab, name='challenges-tab'),
+    path('buy-in-tab/', views.buyInTab, name='buy-in-tab'),
+    path('my-tickets-tab/', views.myTicketsTab, name='my-tickets-tab'),
+    path('battles-tab/', views.battlesTab, name='battles-tab'),
     path('admin-panel/', views.adminPanel, name="admin-panel"),
 ]
--- baseapp/views.py
+++ baseapp/views.py
@@ -1,11 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 
 
 def homePage(request):
-    return render(request=request, template_name="baseapp/base.html")
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        if 'login' in request.POST:
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('/my-profile-tab')
+            else:
+                messages.info(request, 'Invalid Credentials!')
+                return redirect('/')
+        # elif ('register' in request.POST):
+        #     if username == '' or password == '':
+        #         messages.error(
+        #             request, 'Please provide both username and password')
+        #         return redirect('/')
+        #     else:
+        #         if User.objects.filter(username=username).exists():
+        #             messages.info(request, 'Username Already Taken!')
+        #             return redirect('/')
+        #         else:
+        #             user = User.objects.create_user(
+        #                 username=username, password=password)
+        #             user.save()
+        #             messages.info(
+        #                 request, 'Account Registered! Please Login Now')
+        #             return redirect('/')
+    else:
+        return render(request, 'baseapp/base.html')
+
+
+def myProfileTab(request):
+    return render(request, 'baseapp/my-profile-tab.html')
+
+
+def guildsTab(request):
+    return render(request, 'baseapp/guilds-tab.html')
+
+
+def challengesTab(request):
+    return render(request, 'baseapp/challenges-tab.html')
+
+
+def buyInTab(request):
+    return render(request, 'baseapp/buy-in-tab.html')
+
+
+def myTicketsTab(request):
+    return render(request, 'baseapp/my-tickets-tab.html')
+
+
+def battlesTab(request):
+    return render(request, 'baseapp/battles-tab.html')
 
 
 def adminPanel(request):
-    return render(request=request, template_name="baseapp/admin-panel.html")
+    return render(request, 'baseapp/admin-panel.html')
