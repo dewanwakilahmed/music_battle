2cb19ef4080894ae1041f5c30393a808f4ae5aec
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2021-12-30 01:50:32 +0600

    User Model Updated

--- baseapp/models.py
+++ baseapp/models.py
@@ -3,10 +3,15 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    user_dob = models.DateField(blank=True, null=True)
     user_image = models.ImageField(upload_to="user-images", null=True)
+    user_gold = models.IntegerField(blank=True, null=True)
+    user_tickets = models.CharField(max_length=50, blank=True, null=True)
+    user_music = models.CharField(max_length=50, blank=True, null=True)
+    user_rank = models.CharField(max_length=50, blank=True, null=True)
     user_genre = models.CharField(max_length=50, blank=True, null=True)
     user_guild = models.CharField(max_length=50, blank=True, null=True)
-    user_gold = models.IntegerField(blank=True, null=True)
+    user_guild_rank = models.CharField(max_length=50, blank=True, null=True)
     user_xp = models.IntegerField(blank=True, null=True)
     user_hp = models.IntegerField(default=100, null=True)
 
